02a11c07cae49dfb86a32d879f0c0c0763308e2a
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-02-15 22:40:33 +0100

    editG3commentato

--- m1/s3/g3/esercitazioneG3/script/script.js
+++ m1/s3/g3/esercitazioneG3/script/script.js
@@ -1,75 +1,38 @@
-/*
-copiato da progetto online, usato come base di studio nel pomeriggio,
-il mio progetto non riusciva ad inizializzare la classe
-*/
+//qui definisco il costruttore
+class ToDoList{
+  constructor(list){
+      this.todolist = list;
+  }
 
+//qui le funzioni che creano e rimuovono la ul
+      addToList(content){
+      let list = document.querySelector('#ulList');
+      let li = document.createElement("li");
+      li.textContent = content;
+      list.appendChild(li)
+      let delButton = document.createElement('button')
+  }
+  
+  removeFromList(){
+          delButton.addEventListener('click', function(){
+              
+          })
+          li = document.getElementById('li')
+          let updated = document.getElementById("ulList");
+          updated.onclick = elemento.removeFromList(li.innerText)
+      }
+  
+      addToDo(){
+          let content = document.getElementById("input").value;
+          elemento.addToList(content);
+      }
 
-class MakeTodoList {
-    constructor(list) {
-      this.todoList = list;
-      this.todos = [];
-    }
-
-    static addtoList(text) {
-      let list = document.getElementById("todo-list");
-      var li = document.createElement("li");
-      li.textContent = text;
-      list.appendChild(li);
-      return li;
-    }
-
-    static removeFromList(text) {
-      let list = document.getElementById("todo-list");
-      let childs = Array.from(list.childNodes);
-      let item = childs.find(i => i.innerText === text);
-      return item;
-    }
-
-    addTodo(text) {
-      this.todos.push(text);
-      this.todoList.appendChild(MakeTodoList.addtoList(text));
-    }
-
-    removeTodo(text) {
-      let filter = this.todos.filter(i => i !== text);
-      this.todoList.removeChild(MakeTodoList.removeFromList(text));
-      this.todos = filter;
-    }
-
-    get getList() {
-      return this.todos;
-    }
 
 }
-let list = document.getElementById("todo-list");
 
-  // create a object of Class
+let elemento = new ToDoList()
 
-let listEle = new MakeTodoList(list);
+let button = document.getElementById('button')
 
-  // add something to list
-
-function addtodo() {
-    let input = document.getElementById("input").value;
-    listEle.addTodo(input); //  calling method from class
-
-    console.log("current  ...........", listEle.getList); //   getter method
-}
-
-  // simple utility function
-
-function getEventTarget(e) {
-    e = e || window.event;
-    return e.target || e.srcElement;
-}
-
-  // delete some item from list
-
-function remove() {
-    let updated = document.getElementById("todo-list");
-    updated.onclick = function(event) {
-      var target = getEventTarget(event);
-      listEle.removeTodo(target.innerText);
-      console.log("current  ...........", listEle.getList); //   getter method
-    };
-}
+button.addEventListener('click', () => elemento.addToDo())
+//ho studiato di nuovo il costruttore dal vecchio progetto, snellendo il codice e cercando di capire le funzioni passo passo, ne risulta un js più corto e funzionale, manca ancora la funzione per eliminare e il css
