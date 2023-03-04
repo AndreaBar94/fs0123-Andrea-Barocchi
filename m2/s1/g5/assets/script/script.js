75055492e14c36338b945f9bd0898b3e244188c5
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-04 11:24:53 +0100

    progettoM2S1G5

--- m2/s1/g5/assets/script/script.js
+++ m2/s1/g5/assets/script/script.js
@@ -2,7 +2,7 @@ let navbar = document.querySelector('nav');
 let button = document.querySelector('.getStarted');
 
 window.onscroll = function() {
-    if (window.pageYOffset > 200) {
+    if (window.pageYOffset > 300) {
         navbar.classList.add('active')
         button.classList.add('active2')
     } else {
@@ -10,3 +10,23 @@ window.onscroll = function() {
         button.classList.remove('active2')
     }
 };
+
+// ANIMAZIONE LETTERE
+
+let letters = document.querySelectorAll('.animatedM svg g')
+
+function selectRandom(x){
+    return x[Math.floor(Math.random() * x.length)];
+}
+
+// aggiungo classe per farle sparire
+function animateLetter(){
+    let switchTrue = selectRandom(Array.from(letters));
+    if(switchTrue.classList.contains('active3')){
+        switchTrue.classList.remove('active3');
+    }else{
+        switchTrue.classList.add('active3');
+    }
+};
+//attivo tutto
+setInterval(animateLetter, 2);
